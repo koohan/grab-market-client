6a95dce19d8238436d6d5b3507a5e4c23ec58e8c
Dandol <never-alone@naver.com>
2023-04-30 21:38:09 +0900

    antd 기본 적용

--- src/main/index.js
+++ src/main/index.js
@@ -19,45 +19,40 @@ function MainPage(){
       }, []);
     return (
       <div>
-        <div id="header">
-        <div id="header-area">
-            <img src="images/icons/logo.png" alt=""/>
+        <div id="banner">
+          <img src="images/banners/banner1.png" alt=""/>
         </div>
-        </div>
-        <div id="body">
-          <div id="banner">
-            <img src="images/banners/banner1.png" alt=""/>
-          </div>
-          <h1>Selling Products</h1>
-          <div id="product-list">
-            {
-              products.map(function(product, index){
-                return (
-                  <div className='product-card'>
-                    <Link className="product-link" to={`/products/${index}`}>
-                      <div>
-                        <img className='product-img' src={product.imageUrl}/>
-                      </div> 
-                      <div className='product-contents'>
-                        <span className='product-name'>
-                          {product.name}
-                        </span>
-                        <span className='product-price'>
-                          {product.price}Won
-                        </span>
-                        <div className='product-seller'>
-                          <img className='product-avatar' src='images/icons/avatar.png'/>
-                          <span>{product.seller}</span>
-                        </div>
+        <h1>Selling Products</h1>
+        <div id="product-list">
+          {
+            products.map(function(product, index){
+              return (
+                <div className='product-card'>
+                  <Link
+                    style={{color:"inherit"}} 
+                    className="product-link" 
+                    to={`/products/${product.id}`}
+                  >
+                    <div>
+                      <img className='product-img' src={product.imageUrl}/>
+                    </div> 
+                    <div className='product-contents'>
+                      <span className='product-name'>
+                        {product.name}
+                      </span>
+                      <span className='product-price'>
+                        {product.price}Won
+                      </span>
+                      <div className='product-seller'>
+                        <img className='product-avatar' src='images/icons/avatar.png'/>
+                        <span>{product.seller}</span>
                       </div>
-                    </Link>
-                  </div>
-                ) 
-              })
-            }
-          </div>
+                    </div>
+                  </Link>
+                </div>
+              ) 
+            })}
         </div>
-        <div id="footer"></div>
       </div>
     );
 }
--- src/product/index.js
+++ src/product/index.js
@@ -1,8 +1,44 @@
 import {useParams} from 'react-router-dom';
-
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './index.css';
 function ProductPage(){
     const {id} = useParams();
-    return <h1>Product Detail Page {id} product</h1>
+    const [product, setProduct] = useState(null);
+    useEffect(function(){
+    axios
+        .get(
+            `https://cf883f40-256c-40ec-a149-5373ada9d82b.mock.pstmn.io/products/${id}`
+        )
+        .then(function(result){
+            setProduct(result.data);
+        })
+        .catch(function (error){
+            console.error(error);    
+        })
+    },[]);
+
+    if(product === null){
+      return <h1>Product information loading...</h1>
+    }
+
+    return (
+        <div>
+          <div id="image-box">
+            <img src={"/"+product.imageUrl} />
+          </div>  
+          <div id="profile-box">
+            <img src="/images/icons/avatar.png"/>
+            <span>{product.seller}</span>
+          </div>
+          <div id="contents-box">
+            <div id="name">{product.name}</div>
+            <div id="price">{product.price}Won</div>
+            <div id="createdAt">2023-4-30</div>
+            <div id="description">{product.description}</div>
+          </div>  
+        </div>
+    )
 }
 
 export default ProductPage;
